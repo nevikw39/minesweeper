05d19012e555918e439a6e9b8cf068366292a759
nevikw39 <nevikw39@icloud.com>
2020-01-03 13:02:55 +0800

    0103

--- grid.py
+++ grid.py
@@ -9,6 +9,7 @@ from pygame.locals import MOUSEBUTTONDOWN, QUIT, USEREVENT, Color
 
 imgs = [pg.image.load(os.path.join("assets", "%d.png" % i)) for i in range(6)]
 img_flag = pg.image.load(os.path.join("assets", "flag.png"))
+img_default = pg.image.load(os.path.join("assets", "default.png"))
 
 
 class grid(pg.sprite.Sprite):
@@ -19,7 +20,19 @@ class grid(pg.sprite.Sprite):
         self.j = j
         self.n = 0
         self.s = 0
-        self.image = imgs[0]
+        self.image = img_default
         self.is_mine = is_mine
         self.rect = self.image.get_rect()
         self.rect.center = (i * 50 + 25, j * 50 + 25)
+    
+    def right_click(self):
+        if self.s == 0:
+            self.s = 1
+            self.image = img_flag
+        elif self.s == 1:
+            self.s = 0
+            self.image = img_default
+    
+    def left_click(self, n):
+        self.n = n
+        self.image = imgs[n]
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 import random
 import sys
 import time
+from tkinter import *
+from tkinter import messagebox
+Tk().wm_withdraw()
 
 import pygame as pg
 from pygame import *
@@ -16,52 +19,111 @@ FPS = 60
 pg.init()
 
 Cnt = 10
-Lst = []
-Tab = [[] for _ in range(9)]
+Mines = set()
+Flags = set()
+Map = [[] for _ in range(9)]
 Sprites = pg.sprite.Group()
 
 Clock = pg.time.Clock()
 Screen = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-Background  = pg.Surface(Screen.get_size())
+Background = pg.Surface(Screen.get_size())
 Background.convert()
 Background.fill((0, 0, 0))
 pg.display.set_caption("Simple Minesweeper by nevikw39")
 
 
+def init():
+    global Cnt, Mines, Flags
+    Cnt = 10
+    Mines = set(random.sample(range(81), 10))
+    Flags = set()
+    for i, e in enumerate(Map):
+        e.clear()
+        for j in range(9):
+            e.append(grid(i, j, i * 9 + j in Mines))
+            Sprites.add(e[j])
+    print("Mines =", Mines)
+
+
 def dfs(i, j):
-    pass
+    Map[i][j].s = 2;
+    dx = [1, 1, 0, -1, -1, -1, 0, 1]
+    dy = [0, 1, 1, 1, 0, -1, -1, -1]
+    n = 0
+
+    for k in range(8):
+        x = i + dx[k]
+        y = j + dy[k]
+        if not 0 <= x < 9 or not 0 <= y < 9:
+            continue;
+        elif Map[x][y].is_mine:
+            n += 1
+    
+    if not n:
+        for k in range(8):
+            x = i + dx[k]
+            y = j + dy[k]
+            if 0 <= x < 9 and 0 <= y < 9 and Map[x][y].s == 0:
+                dfs(x, y)
+    
+    Map[i][j].left_click(n)
+
+
+def gg():
+    if messagebox.askyesno('GG','你超爛ＱＱ\n\n再來一局ㄇ？？', icon='error'):
+        init()
+        return True
+    else:
+        pg.quit()
+        return False
+
+
+def win():
+    if messagebox.askyesno('Win','你贏惹 o\'_\'o\n\n再來一局ㄇ？？', icon='info'):
+        init()
+        return True
+    else:
+        pg.quit()
+        return False
 
 
 def main():
-    global Cnt, Lst
-    Lst = random.sample(range(81), 10)
-    for i, e in enumerate(Tab):
-        for j in range(9):
-            e.append(grid(i, j, i * 9 + j in Lst))
-            Sprites.add(e[j])
-
-    running = True
-    while running:
+    global Cnt
+    init()
+    while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                running = False
-        btns = pg.mouse.get_pressed()
-        i, j = pg.mouse.get_pos()[0] // 50, pg.mouse.get_pos()[1] // 50
-        if btns[0]:
-            dfs(i, j)
-        elif btns[2]:
-            if Tab[i][j].s == 0:
-                Tab[i][j].s = 1
-                Tab[i][j].image = img_flag
-                Cnt -= 1
-            elif Tab[i][j].s == 1:
-                Tab[i][j].s = 0
-                Tab[i][j].image = imgs[0]
-                Cnt += 1
+                pg.quit()
+                return
+
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                i, j = event.pos[0] // 50, event.pos[1] // 50
+                if event.button == 1:
+                    if (i * 9 + j in Mines):
+                        if gg():
+                            continue
+                        else:
+                            return
+                    dfs(i, j)
+                elif event.button == 3:
+                    if Map[i][j].s == 2:
+                        continue
+                    elif Map[i][j].s == 1:
+                        Flags.remove(i * 9 + j)
+                        Cnt += 1
+                    elif Map[i][j].s == 0:
+                        Flags.add(i * 9 + j)
+                        Cnt -= 1
+                    Map[i][j].right_click()
+                    if not Cnt and Flags == Mines:
+                        if win():
+                            continue
+                        else:
+                            return
+        
         Sprites.draw(Screen)
         pg.display.update()
         Clock.tick(FPS)
-    pg.quit()
 
 
 if __name__ == '__main__':
